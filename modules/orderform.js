ea8b5461e74c765e634f4fed453ee453e0f2d488
Byron Rios <info@matematico.pro>
2016-01-25 23:59:23 +0300

    fixed orderform search

--- modules/orderform.js
+++ modules/orderform.js
@@ -47,16 +47,54 @@ module.exports = function (sequelize, app, multipartMiddleware, opts) {
   
   app.post('/api/orderform/upload', multipartMiddleware, orderform.upload);
   app.get('/api/orderform', function (req, res) {
-    var search = { $or: { }};
-    if (req.query) {
-      if ((req.query.search) && (req.query.search.length > 0)) {
-        for (var attr in Model.attributes) {
-          if (attr === 'id') continue;
-          search.$or[attr] = sequelize.where(sequelize.cast(sequelize.col(attr), 'text'), { $ilike: '%' + req.query.search + '%' });
+    return orderform.test(req, res).then(function() {
+      var search = { $or: { } },
+          attributes = [
+            "order_form.id",
+            "order_form.comments",
+            "order_form.discount",
+            "order_form.prepayment",
+            "order_form.status",
+            "order_form.author",
+            "order_form.commercialProposalId",
+            "commercial_proposal.comments",
+            "commercial_proposal.sent_date",
+            "commercial_proposal.alert_date",
+            "commercial_proposal.status",
+            "commercial_proposal.author",
+            "commercial_proposal.client.first_name",
+            "commercial_proposal.client.middle_name",
+            "commercial_proposal.client.last_name",
+            "commercial_proposal.client.birth_date",
+            "commercial_proposal.client.passport",
+            "commercial_proposal.client.telephone",
+            "commercial_proposal.client.address",
+            "commercial_proposal.client.email",
+            "commercial_proposal.client.comments",
+            "commercial_proposal.broker.first_name",
+            "commercial_proposal.broker.middle_name",
+            "commercial_proposal.broker.last_name",
+            "commercial_proposal.broker.birth_date",
+            "commercial_proposal.broker.passport",
+            "commercial_proposal.broker.telephone",
+            "commercial_proposal.broker.address",
+            "commercial_proposal.broker.email",
+            "commercial_proposal.broker.type",
+            "commercial_proposal.broker.organization_name",
+            "commercial_proposal.broker.legal_name",
+            "commercial_proposal.broker.inn",
+            "commercial_proposal.broker.requeriments",
+            "commercial_proposal.broker.comments",
+            "commercial_proposal_variant.name",
+            "commercial_proposal_variant.comments"
+          ];
+      if (req.query) {
+        if ((req.query.search) && (req.query.search.length > 0)) {
+          attributes.forEach(function(attr) {
+            search.$or[attr] = sequelize.where(sequelize.cast(sequelize.col(attr), 'text'), { $ilike: '%' + req.query.search + '%' });
+          });
         }
       }
-    }
-    orderform.test(req, res).then(function() {
       return OrderForm.findAll({
         where: search,
         include: [{
@@ -73,7 +111,6 @@ module.exports = function (sequelize, app, multipartMiddleware, opts) {
         res.json(entries);
       });
     });
-    
   });
   app.get('/api/orderform/:id', function (req, res) {
     var id = ~~req.params.id;
